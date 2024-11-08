33cf27ed8e49fd6a541eed3952c7879b95de5c2d
anton13samarin <big13sav@gmail.com>
2024-11-08 09:42:35 +0300

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,7 @@
+my_string = str('Привет. Как дела?')
+print(len( my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(str(my_string) .replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
